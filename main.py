ea841eeb7f88b8dd1943983551904f3c1dbb4f32
Dhruv <dhruvramani98@gmail.com>
2019-11-15 21:39:59 +0530

    debug

--- main.py
+++ main.py
@@ -97,8 +97,7 @@ def train_eval(tf_agent, tf_env, eval_tf_env, num_iterations=2000000,
 		time_acc += time.time() - start_time
 
 		if global_step.numpy() % log_interval == 0:
-			tf.logging.info('step = %d, loss = %f', global_step.numpy(),
-										train_loss.loss)
+			tf.logging.info('step = %d, loss = %f', global_step.numpy(), train_loss.loss)
 			steps_per_sec = log_interval / time_acc
 			tf.logging.info('%.3f steps/sec', steps_per_sec)
 			time_acc = 0
@@ -128,8 +127,7 @@ def train_eval(tf_agent, tf_env, eval_tf_env, num_iterations=2000000,
 					ts = eval_tf_env.reset()
 					dist_to_goal = agent._get_dist_to_goal(ts)[0]
 					pred_dist.append(dist_to_goal.numpy())
-				tf.logging.info('\t\t predicted_dist = %.1f (%.1f)' %
-												(np.mean(pred_dist), np.std(pred_dist)))
+				tf.logging.info('\t\t predicted_dist = %.1f (%.1f)' % (np.mean(pred_dist), np.std(pred_dist)))
 			tf.logging.info('\t eval_time = %.2f' % (time.time() - start))
 				
 	return train_loss
@@ -165,10 +163,7 @@ train_eval(
 eval_tf_env.pyenv.envs[0]._duration = 100  # We'll give the agent lots of time to try to find the goal.
 difficulty = 0.8 #@param {min:0, max: 1, step: 0.1, type:"slider"}
 max_goal_dist = eval_tf_env.pyenv.envs[0].gym.max_goal_dist
-eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
-	prob_constraint=1.0,
-	min_dist=max(0, max_goal_dist * (difficulty - 0.05)),
-	max_dist=max_goal_dist * (difficulty + 0.05))
+eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(prob_constraint=1.0, min_dist=max(0, max_goal_dist * (difficulty - 0.05)), max_dist=max_goal_dist * (difficulty + 0.05))
 
 
 def get_rollout(tf_env, policy, seed=None):
@@ -196,12 +191,9 @@ for col_index in range(2):
 	obs_vec, goal, _ = get_rollout(eval_tf_env, agent.policy)
 
 	plt.plot(obs_vec[:, 0], obs_vec[:, 1], 'b-o', alpha=0.3)
-	plt.scatter([obs_vec[0, 0]], [obs_vec[0, 1]], marker='+',
-				color='red', s=200, label='start')
-	plt.scatter([obs_vec[-1, 0]], [obs_vec[-1, 1]], marker='+',
-				color='green', s=200, label='end')
-	plt.scatter([goal[0]], [goal[1]], marker='*',
-				color='green', s=200, label='goal')
+	plt.scatter([obs_vec[0, 0]], [obs_vec[0, 1]], marker='+', color='red', s=200, label='start')
+	plt.scatter([obs_vec[-1, 0]], [obs_vec[-1, 1]], marker='+', color='green', s=200, label='end')
+	plt.scatter([goal[0]], [goal[1]], marker='*', color='green', s=200, label='goal')
 	if col_index == 0:
 	plt.legend(loc='lower left', bbox_to_anchor=(0.3, 1), ncol=3, fontsize=16)
 
@@ -280,10 +272,7 @@ search_policy = SearchPolicy(agent, open_loop=True)
 # --------- Search Path. ---------
 difficulty = 0.6 #@param {min:0, max: 1, step: 0.1, type:"slider"}
 max_goal_dist = eval_tf_env.pyenv.envs[0].gym.max_goal_dist
-eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(
-		prob_constraint=1.0,
-		min_dist=max(0, max_goal_dist * (difficulty - 0.05)),
-		max_dist=max_goal_dist * (difficulty + 0.05))
+eval_tf_env.pyenv.envs[0].gym.set_sample_goal_args(prob_constraint=1.0, min_dist=max(0, max_goal_dist * (difficulty - 0.05)), max_dist=max_goal_dist * (difficulty + 0.05))
 ts = eval_tf_env.reset()
 start = ts.observation['observation'].numpy()[0]
 goal = ts.observation['goal'].numpy()[0]
@@ -298,10 +287,8 @@ for waypoint_index in search_policy._waypoint_vec:
 waypoint_vec.append(goal)
 waypoint_vec = np.array(waypoint_vec)
 
-plt.scatter([start[0]], [start[1]], marker='+',
-						color='red', s=200, label='start')
-plt.scatter([goal[0]], [goal[1]], marker='*',
-						color='green', s=200, label='goal')
+plt.scatter([start[0]], [start[1]], marker='+', color='red', s=200, label='start')
+plt.scatter([goal[0]], [goal[1]], marker='*', color='green', s=200, label='goal')
 plt.plot(waypoint_vec[:, 0], waypoint_vec[:, 1], 'y-s', alpha=0.3, label='waypoint')
 plt.legend(loc='lower left', bbox_to_anchor=(-0.1, -0.15), ncol=4, fontsize=16)
 plt.show()
@@ -330,8 +317,7 @@ for col_index in range(2):
 	goal = ts.observation['goal'].numpy()[0]
 	start = ts.observation['observation'].numpy()[0]
 	obs_vec = []
-	for _ in tqdm.tnrange(eval_tf_env.pyenv.envs[0]._duration,
-												desc='rollout %d / 2' % (col_index + 1)):
+	for _ in tqdm.tnrange(eval_tf_env.pyenv.envs[0]._duration, desc='rollout %d / 2' % (col_index + 1)):
 		if ts.is_last():
 			break
 		obs_vec.append(ts.observation['observation'].numpy()[0])
